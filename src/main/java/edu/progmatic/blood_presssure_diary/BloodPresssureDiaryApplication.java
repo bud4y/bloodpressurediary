0526df1c9ed366088eb2a3b8ea9a22fb82ca110a
Botos János <botosjano@gmail.com>
2020-07-14 11:12:32 +0200

    initial commit

---
+++ src/main/java/edu/progmatic/blood_presssure_diary/BloodPresssureDiaryApplication.java
@@ -0,0 +1,13 @@
+package edu.progmatic.blood_presssure_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodPresssureDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloodPresssureDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/progmatic/blood_presssure_diary/BloodPresssureDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.progmatic.blood_presssure_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodPresssureDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
