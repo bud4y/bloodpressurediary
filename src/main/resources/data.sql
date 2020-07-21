492329228f3cb316227c9a7bb2a309cb0a343297
Botos János <botosjano@gmail.com>
2020-07-21 22:27:10 +0200

    Role added to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 # INSERT INTO `bloodpressure`.`user` (`bmi`, `birth_date`, `email`, `first_name`, `height`, `is_male`, `last_name`, `password`, `weight`) VALUES (20, '1991-06-08', 'botosjano@gmail.com', 'János', 183, 1, 'Botos', 'asdf', 75);
-# INSERT INTO `bloodpressure`.`user` (`bmi`, `birth_date`, `email`, `first_name`, `height`, `is_male`, `last_name`, `password`, `weight`) VALUES (20, '1992-12-08', 'tcsaba92@gmail.com', 'Csaba', 183, 1, 'Tóth', 'asdf', 75);
+# INSERT INTO `bloodpressure`.`user` (`birth_date`, `email`, `first_name`, `height`, `is_male`, `last_name`, `password`, `weight`, `username`) VALUES ('1992-12-25', 'user@gmail.com', 'User', 183, 1, 'User', 'Password1234!', 100, 'user');
 INSERT INTO `bloodpressure`.`evaluate` (name, text) VALUES ('Nagyszerű hírünk van! Önnek tökéletes a vérnyomása.', 'Optimális vérnyomás');
 INSERT INTO `bloodpressure`.`evaluate` (name, text) VALUES ('Önnek normális a vérnyomása.', 'Normális vérnyomás.');
 INSERT INTO `bloodpressure`.`evaluate` (name, text) VALUES ('Emelkedett-normál szisztolés vérnyomás.', 'Emelkedett');
