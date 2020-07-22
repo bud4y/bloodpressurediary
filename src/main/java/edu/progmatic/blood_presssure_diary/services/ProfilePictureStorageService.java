ee55a3b8b81c2c64e1d3c423a7cf2eeefce1b8ea
Toth Csaba <toth.csaba.szeged@gmail.com>
2020-07-22 13:39:47 +0200

    profile picture storage compress/decompress

--- src/main/java/edu/progmatic/blood_presssure_diary/services/ProfilePictureStorageService.java
+++ src/main/java/edu/progmatic/blood_presssure_diary/services/ProfilePictureStorageService.java
@@ -14,7 +14,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 @Service
 public class ProfilePictureStorageService {
@@ -35,7 +40,7 @@ public class ProfilePictureStorageService {
                 throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
             }
 
-            ProfilePicture profilePicture = new ProfilePicture(fileName, file.getContentType(), file.getBytes());
+            ProfilePicture profilePicture = new ProfilePicture(fileName, file.getContentType(), compressBytes(file.getBytes()));
             profilePictureRepository.save(profilePicture);
             Authentication auth = SecurityContextHolder.getContext().getAuthentication();
             Object principal = auth.getPrincipal();
@@ -49,8 +54,28 @@ public class ProfilePictureStorageService {
         }
     }
 
-    public ProfilePicture getFile(String fileId) {
+    public ProfilePicture getFile(Integer fileId) {
         return profilePictureRepository.findById(fileId)
                 .orElseThrow(() -> new FileNotFoundException("File not found with id " + fileId));
     }
+
+    public static byte[] compressBytes(byte[] data) {
+        Deflater deflater = new Deflater();
+        deflater.setInput(data);
+        deflater.finish();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        while (!deflater.finished()) {
+            int count = deflater.deflate(buffer);
+            outputStream.write(buffer, 0, count);
+        }
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+        }
+        System.out.println("Compressed Image Byte Size - " + outputStream.toByteArray().length);
+        return outputStream.toByteArray();
+    }
+
+
 }
