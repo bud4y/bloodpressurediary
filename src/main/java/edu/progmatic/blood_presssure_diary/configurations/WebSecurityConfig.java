0aaef74be22c534a4393b1b7a223d176d47439f2
Budai Bence <budaibence@yahoo.com>
2020-07-24 10:57:08 +0200

    add emailservices

--- src/main/java/edu/progmatic/blood_presssure_diary/configurations/WebSecurityConfig.java
+++ src/main/java/edu/progmatic/blood_presssure_diary/configurations/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import edu.progmatic.blood_presssure_diary.services.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -66,7 +67,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     }
                 })
                 .and()
-                .csrf().disable().authorizeRequests().antMatchers(PUBLIC_MATCHERS)
+                .csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS,"/**","/css/*", "/js/*", "/images/*","/favicon.ico","/login","/listOfTopics","/createMessage","/user/login", "/user/register","/user/**","user/{id}", "/create","/newTopic")
                 .permitAll()
         .anyRequest().authenticated()
         ;
@@ -76,17 +77,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        configuration.setAllowedMethods(Arrays.asList("HEAD",
-                "GET", "POST", "PUT", "DELETE", "PATCH"));
-        // setAllowCredentials(true) is important, otherwise:
-        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowCredentials(true);
-        // setAllowedHeaders is important! Without it, OPTIONS preflight request
-        // will fail with 403 Invalid CORS request
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         return source;
     }
 }
